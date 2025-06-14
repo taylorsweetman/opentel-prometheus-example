585abd925522126300805537dcb43430fcb74728
Taylor Sweetman <taylor.sweetman@tactable.io>
2025-06-14 09:44:34 -0400

    Init example project

---
+++ src/main/java/org/example/ExampleConfiguration.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import static io.opentelemetry.semconv.ServiceAttributes.SERVICE_NAME;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.exporter.logging.LoggingSpanExporter;
+import io.opentelemetry.exporter.prometheus.PrometheusHttpServer;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.metrics.SdkMeterProvider;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
+
+public final class ExampleConfiguration {
+
+    /**
+     * Initializes the OpenTelemetry SDK and configures the prometheus collector with all default
+     * settings.
+     *
+     * @param prometheusPort the port to open up for scraping.
+     * @return a ready-to-use {@link OpenTelemetry} instance.
+     */
+    static OpenTelemetry initOpenTelemetry(int prometheusPort) {
+        // Include required service.name resource attribute on all spans and metrics
+        Resource resource =
+                Resource.getDefault()
+                        .merge(Resource.builder().put(SERVICE_NAME, "PrometheusExporterExample").build());
+
+        OpenTelemetrySdk openTelemetrySdk =
+                OpenTelemetrySdk.builder()
+                        .setTracerProvider(
+                                SdkTracerProvider.builder()
+                                        .setResource(resource)
+                                        .addSpanProcessor(SimpleSpanProcessor.create(LoggingSpanExporter.create()))
+                                        .build())
+                        .setMeterProvider(
+                                SdkMeterProvider.builder()
+                                        .setResource(resource)
+                                        .registerMetricReader(
+                                                PrometheusHttpServer.builder().setPort(prometheusPort).build())
+                                        .build())
+                        .buildAndRegisterGlobal();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(openTelemetrySdk::close));
+
+        return openTelemetrySdk;
+    }
+}
---
+++ src/main/java/org/example/PrometheusExample.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.metrics.LongCounter;
+import io.opentelemetry.api.metrics.LongHistogram;
+import io.opentelemetry.api.metrics.Meter;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
+
+/**
+ * Example of using the PrometheusHttpServer to convert OTel metrics to Prometheus format and expose
+ * these to a Prometheus instance via a HttpServer exporter.
+ *
+ * <p>A Gauge is used to periodically measure how many incoming messages are awaiting processing.
+ * The Gauge callback gets executed every collection interval.
+ */
+public final class PrometheusExample {
+    public static void main(String[] args) throws InterruptedException {
+        int prometheusPort = 0;
+        try {
+            prometheusPort = Integer.parseInt(args[0]);
+        } catch (Exception e) {
+            System.out.println("Port not set, or is invalid. Exiting");
+            System.exit(1);
+        }
+        // it is important to initialize your SDK as early as possible in your application's lifecycle
+        OpenTelemetry openTelemetry = ExampleConfiguration.initOpenTelemetry(prometheusPort);
+
+        Tracer tracer = openTelemetry.getTracer("io.opentelemetry.example.prometheus");
+        Meter meter = openTelemetry.getMeter("io.opentelemetry.example.prometheus");
+        LongCounter counter = meter.counterBuilder("example.counter").build();
+        LongHistogram histogram = meter.histogramBuilder("super.timer").ofLongs().setUnit("ms").build();
+
+        for (int i = 0; i < 500; i++) {
+            long startTime = System.currentTimeMillis();
+            Span exampleSpan = tracer.spanBuilder("exampleSpan").startSpan();
+            Context exampleContext = Context.current().with(exampleSpan);
+            try (Scope _ignored = exampleContext.makeCurrent()) {
+                counter.add(1);
+                exampleSpan.setAttribute("good", true);
+                exampleSpan.setAttribute("exampleNumber", i);
+                Thread.sleep(1000);
+            } finally {
+                histogram.record(
+                        System.currentTimeMillis() - startTime, Attributes.empty(), exampleContext);
+                exampleSpan.end();
+            }
+        }
+
+        System.out.println("Exiting");
+    }
+}
